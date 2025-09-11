86e6412f653acbea794f3018073e6a3253e2fb55
Tommaso Fiorillo <tommyfiore97@gmail.com>
2025-09-11 23:18:58 +0000

    5th

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
-    required providers{
+    required_providers {
         aws = {
-            source ="hashicorp/aws"
-            vbersion = "~=4.0
+            source = "hashicorp/aws"
+            version = "~>4.0"
         }
     }
     backend "s3"{
@@ -16,7 +16,7 @@ provider "aws" {
 }
 resource "aws_instance" "server"{
     ami = "ami-0ae0a4f69bf1ea649"
-    instance.type = "t2.micro"
+    instance_type = "t2.micro"
     key_name = aws_key_pair.deployer.key_name
     vpc_security_group_ids = [aws_security_group.maingroup.id]
     iam_instance_profile = aws_iam_instance_profile.ec2-profile.name 
@@ -33,51 +33,48 @@ resource "aws_instance" "server"{
 }
 resource "aws_iam_instance_profile" "ec2-profile" {
     name = "ec2-profile"
-    role = "EC2-ECR-AUTH" #the name of the role you have to set it personally in AWS
+    role = "EC2-ECR-AUTH" # The IAM role "EC2-ECR-AUTH" must exist in AWS before applying this configuration
 }
 resource "aws_security_group" "maingroup" {
-    egress = {
-        {
-            cdir_blocs = ["0.0.0.0/0"]
-            description = ""
-            from_port = 0
-            ipv6_cidr_blocks = []
-            prefix_list_ids ) []
-            protocol = "-1"
-            security_groups = []
-            self = false
-            to_port = 0
-        }
-}
-    ingress = {
-        {
-            cdir_blocs = ["0.0.0.0/0"]
-            description = ""
-            from_port = 22
-            ipv6_cidr_blocks = []
-            prefix_list_ids ) []
-            protocol = "tcp"
-            security_groups = []
-            self = false
-            to_port = 22 #traffico ssh
-        },
-        {
-            cdir_blocs = ["0.0.0.0/0"]
-            description = ""
-            from_port = 80
-            ipv6_cidr_blocks = []
-            prefix_list_ids ) []
-            protocol = "tcp"
-            security_groups = []
-            self = false
-            to_port = 80
-        }
+    egress {
+        cidr_blocks = ["0.0.0.0/0"]
+        from_port = 0
+        ipv6_cidr_blocks = []
+        prefix_list_ids = []
+        protocol = "-1"
+        security_groups = []
+        self = false
+        to_port = 0
+    }
+
+    ingress {
+        cidr_blocks = ["0.0.0.0/0"]
+        description = ""
+        from_port = 22
+        ipv6_cidr_blocks = []
+        prefix_list_ids = []
+        protocol = "tcp"
+        security_groups = []
+        self = false
+        to_port = 22 #traffico ssh
+    }
+
+    ingress {
+        cidr_blocks = ["0.0.0.0/0"]
+        description = ""
+        from_port = 80
+        ipv6_cidr_blocks = []
+        prefix_list_ids = []
+        protocol = "tcp"
+        prefix_list_ids = []
+        to_port = 80
+    }
 
 resource "aws_key_pair" "deployer"{
-    key_name =var.key_name
+resource "aws_key_pair" "deployer"{
+    key_name = var.key_name
     public_key = var.public_key
 }
-
 output "instance_public_ip" {
     value = aws_instance.server.public_ip
     sensitive = true
